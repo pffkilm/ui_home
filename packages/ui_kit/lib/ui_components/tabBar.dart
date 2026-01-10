3573cdab93bea335f9a7cf5abb163bf4486dc2dc
Your Name <you@example.com>
2026-01-11 00:45:57 +0300

    TabBar изменила

--- packages/ui_kit/lib/ui_components/tabBar.dart
+++ packages/ui_kit/lib/ui_components/tabBar.dart
@@ -1,25 +1,23 @@
-import 'package:flutter/material.dart';
 import 'package:ui_kit/ui_kit.dart';
+import 'package:flutter/material.dart';
 
 class BottomNavigationHelper {
   Widget create({int initialIndex = 0}) {
-    return _BottomNavigationWidget(initialIndex: initialIndex);
+    return _ButtonWidget(initialIndex: initialIndex);
   }
 }
 
 
-class _BottomNavigationWidget extends StatefulWidget {
+class _ButtonWidget extends StatefulWidget {
   final int initialIndex;
 
-  const _BottomNavigationWidget({
-    required this.initialIndex
-  });
+  const _ButtonWidget({super.key, required this.initialIndex});
 
   @override
-  State<_BottomNavigationWidget> createState() => _BottomState();
+  State<_ButtonWidget> createState() => _buttonState();
 }
 
-class _BottomState extends State<_BottomNavigationWidget> {
+class _buttonState extends State<_ButtonWidget> {
   late int _currentIndex;
 
   @override
@@ -33,29 +31,36 @@ class _BottomState extends State<_BottomNavigationWidget> {
     return BottomNavigationBar(
       backgroundColor: Colors.white,
       currentIndex: _currentIndex,
-      onTap:
-          (index) => setState(() => _currentIndex = index),
       type: BottomNavigationBarType.fixed,
+      onTap: (index) => setState(() => _currentIndex = index),
       selectedItemColor: ui.colors.accent,
       unselectedItemColor: ui.colors.inputBackground,
       selectedLabelStyle: ui.typography.caption2Regular,
       unselectedLabelStyle: ui.typography.caption2Regular,
       items: [
         BottomNavigationBarItem(
-          icon: _currentIndex == 0 ? ui.images.home(size: 32, color: ui.colors.accent) : ui.images.home(size: 32),
-          label: 'Главная',
+          icon: _currentIndex == 0
+              ? ui.images.home(size: 32, color: ui.colors.accent)
+              : ui.images.home(size: 32),
+          label: "Главная",
         ),
         BottomNavigationBarItem(
-          icon: _currentIndex == 1 ? ui.images.katalog(size: 32, color: ui.colors.accent) : ui.images.katalog(size: 32),
-          label: 'Каталог',
+          icon: _currentIndex == 1
+              ? ui.images.katalog(size: 32, color: ui.colors.accent)
+              : ui.images.katalog(size: 32),
+          label: "Каталог",
         ),
         BottomNavigationBarItem(
-          icon: _currentIndex == 2 ? ui.images.project(size: 32, color: ui.colors.accent) : ui.images.project(size: 32),
-          label: 'Проекты',
+          icon: _currentIndex == 2
+              ? ui.images.project(size: 32, color: ui.colors.accent)
+              : ui.images.project(size: 32),
+          label: "Проекты",
         ),
         BottomNavigationBarItem(
-          icon: _currentIndex == 3 ? ui.images.profile(size: 32, color: ui.colors.accent) : ui.images.profile(size: 32),
-          label: 'Профиль',
+          icon: _currentIndex == 3
+              ? ui.images.profile(size: 32, color: ui.colors.accent)
+              : ui.images.profile(size: 32),
+          label: "Профиль",
         ),
       ],
     );
