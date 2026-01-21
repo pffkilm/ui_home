1048b214e75c96449f4f61424d8b08fed9abaaa5
Your Name <you@example.com>
2026-01-21 22:20:21 +0300

    кароточку там сделаем

--- packages/ui_kit/lib/ui_components/cartPrice.dart
+++ packages/ui_kit/lib/ui_components/cartPrice.dart
@@ -1,30 +1,30 @@
-import 'package:flutter/material.dart';
-import 'package:ui_kit/ui_kit.dart';
-
-class CartPrice {
-  Widget cart({required String price}) {
-    return Container(
-      width: 335,
-      height: 56,
-      decoration: BoxDecoration(
-        color: ui.colors.accent,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      padding: EdgeInsets.all(16),
-      child: Row(
-        children: [
-          ui.images.cart(size: 20, color: ui.colors.white),
-          SizedBox(width: 16),
-          Text(
-            "В корзину",
-            style: ui.typography.title3Semibold.copyWith(
-              color: ui.colors.white,
-            ),
-          ),
-          Spacer(),
-          Text('$price ₽',style: ui.typography.title3Semibold.copyWith(color: ui.colors.white), )
-        ],
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:ui_kit/ui_kit.dart';
+//
+// class CartPrice {
+//   Widget cart({required String price}) {
+//     return Container(
+//       width: 335,
+//       height: 56,
+//       decoration: BoxDecoration(
+//         color: ui.colors.accent,
+//         borderRadius: BorderRadius.circular(10),
+//       ),
+//       padding: EdgeInsets.all(16),
+//       child: Row(
+//         children: [
+//           ui.images.cart(size: 20, color: ui.colors.white),
+//           SizedBox(width: 16),
+//           Text(
+//             "В корзину",
+//             style: ui.typography.title3Semibold.copyWith(
+//               color: ui.colors.white,
+//             ),
+//           ),
+//           Spacer(),
+//           Text('$price ₽',style: ui.typography.title3Semibold.copyWith(color: ui.colors.white), )
+//         ],
+//       ),
+//     );
+//   }
+// }
